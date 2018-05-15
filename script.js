3816213e6f366e8a9dc8fde0b8dd31c4ade47d76
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-15 11:34:11 -0400

    2 second timer

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hey ninjas');
+var time = 0;
+
+var timer = setInterval(function() {
+    time += 2;
+    console.log(time + " seconds have passed");
+    if (time > 5) {
+        clearInterval(timer);
+    }
+}, 2000);
