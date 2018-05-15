827f2c509165bb3338a6380c8cfad98c1f59b18a
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-15 11:43:53 -0400

    finished tutorials 1-5

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-var time = 0;
+// TUTORIAL 5 ↓ //
 
-var timer = setInterval(function() {
-    time += 2;
-    console.log(time + " seconds have passed");
-    if (time > 5) {
-        clearInterval(timer);
-    }
-}, 2000);
+// Function Expressions
+
+var sayBye = function(){
+    console.log("bye");
+};
+
+sayBye();
+
+// TUTORIALS 1-4 ↓ //
+
+// Directory name and File name
+
+// console.log(__dirname + " & " + __filename);
+
+// 2 second timer
+
+// var time = 0;
+
+// var timer = setInterval(function() {
+//     time += 2;
+//     console.log(time + " seconds have passed");
+//     if (time > 5) {
+//         clearInterval(timer);
+//     }
+// }, 2000);
