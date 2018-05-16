d6a0c22502bd465beda1fbede95d8df3d9fd260a
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-15 23:19:25 -0400

    finished 12: created a server

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// TUTORIAL 12 ↓ //
+
+// Creating a server //
+
+var http = require("http");
+
+var server = http.createServer(function(req, res){
+    console.log('Request was made: ' + req.url)
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey ninjas');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Hey, now listening to port 3000');
+
 // TUTORIAL 11 ↓ //
 
 // Clients & Servers //
