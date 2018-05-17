ee55e6a94e5e4ab665a68e84330c2dc097244e50
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-17 13:40:53 -0400

    finished 20: node package manager

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// TUTORIAL 19 ↓ //
+// TUTORIAL 20 ↓ //
 
-// Basic Routing //
-
-var http = require("http");
-var fs = require("fs");
+// Node Package Manager (npm) //
 
 var server = http.createServer(function(req, res){
     console.log('Request was made: ' + req.url);
@@ -32,6 +29,41 @@ var server = http.createServer(function(req, res){
 
 server.listen(3000, '127.0.0.1');
 console.log('Hey, now listening to port 3000');
+
+// TUTORIAL 19 ↓ //
+
+// Basic Routing //
+
+// var http = require("http");
+// var fs = require("fs");
+
+// var server = http.createServer(function(req, res){
+//     console.log('Request was made: ' + req.url);
+//     if (req.url === '/home' || req.url === '/') {
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/index.html').pipe(res);
+//     } else if (req.url === '/contact'){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/contact.html').pipe(res);
+//     } else if (req.url === '/api') {
+//         var api = [{
+//             name: 'Ryu',
+//             age: 29
+//         },
+//             {
+//             name: 'Yoshi',
+//             age: 32
+//             }];
+//         res.writeHead(200, {'Content-Type': 'application/JSON'});
+//         res.end(JSON.stringify(api));
+//     } else {
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/404.html').pipe(res);
+//     }
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('Hey, now listening to port 3000');
 
 // // TUTORIAL 18 ↓ //
 
