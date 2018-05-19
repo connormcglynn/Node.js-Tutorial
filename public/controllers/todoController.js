e54a1542b6fcf5e8011b0648c2f520b06f2ce425
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-19 14:00:03 -0400

    finished 37 and final tutorial: Todo List app is complete!

--- public/controllers/todoController.js
+++ public/controllers/todoController.js
@@ -12,29 +12,38 @@ var todoSchema = new mongoose.Schema({
 //Create a todo model based on Schema
 var Todo = mongoose.model('Todo', todoSchema);
 
-//test
-var itemOne = Todo({item: 'buy flowers'}).save(function(err){
-    if (err) throw err;
-    console.log('item saved');
-});
+// //test
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+//     if (err) throw err;
+//     console.log('item saved');
+// });
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+//default todo items
+//var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
+        //get data from mongoDB and pass it to the view
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json(data);
+        //get data from the view and add it to mongoDB
+        var newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.json(data);
+        })
     });
 
     app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo) {
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        //delete requested item from mongoDB
+        Todo.find({item: req.params.item.replace(/\-/g, ' ')}).remove(function(err, data){
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
     });
 };
