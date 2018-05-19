a8d431e973c7b2d1574a7271d95bc27e968a5800
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-19 13:49:54 -0400

    finished 35-36: todo part 5

--- public/controllers/todoController.js
+++ public/controllers/todoController.js
@@ -1,4 +1,22 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to database
+mongoose.connect('mongodb://test:test@ds229290.mlab.com:29290/todo-mcglynn');
+
+//Create a schema -- this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+//Create a todo model based on Schema
+var Todo = mongoose.model('Todo', todoSchema);
+
+//test
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
